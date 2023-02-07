16cbf3719309b1de71a5373b29c10079e1286760
Rodrigo Freitas <rodrigo.freitas@kobe.io>
2023-02-07 18:13:30 -0300

    dependency ascii faces

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log('New feature')
+    res.send("Hello World");
 })
 
-app.get('/feature2', (req, res) => {
-    console.log('New feature2')
-})
-
-app.get('/', (req, res) => {
-    console.log('New feature3')
+app.get('/cool', (req, res) => {
+    res.send(cool());
 })
 
 app.listen(port, () => {
